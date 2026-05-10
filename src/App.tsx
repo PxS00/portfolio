6320dd34aa6239b833a34e6c04eff5f361628f1f
PxS <lucas.rossonidd@gmail.com>
2026-05-09 22:06:02 -0300

    fix(router): scroll to top on route change
    - Create ScrollToTop shared component
    - Integrate in App layout before Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import Footer from './shared/components/Footer/Footer'
 import Navbar from './shared/components/Navbar/Navbar'
+import ScrollToTop from './shared/components/ScrollToTop/ScrollToTop'
 
 export default function App() {
   return (
     <div className="relative flex min-h-screen flex-col bg-transparent">
+      <ScrollToTop />
       <Navbar />
       <main className="w-full flex-1">
         <Outlet />
